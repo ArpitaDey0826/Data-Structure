568378fc143a743770f5b6a2cf61669f9c34266a
DEY, ARPITA <136344442+ArpitaDey0826@users.noreply.github.com>
2024-06-11 21:52:17 +0600

    Add files via upload

---
+++ bouble.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[10]={11,22,33,44,55,66,77,88,91,99};
+    int position=-1 , int item;
+    int f_index=0;
+    int l_index=9;
+    int m_index;
+    first=0;
+
+}
